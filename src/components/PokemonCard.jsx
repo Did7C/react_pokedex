3b09a8be2ebbc22cfb5975cbad3f17a494905e67
Echeverria-Didier <didbidean@hotmail.com>
2023-10-13 11:40:51 +0200

    réonse de la qête 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 
 function PokemonCard() {
     
-    
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
+  return (
+    <figure>
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bubasaur" />
+    <figcaption>Bulbasaur</figcaption>
+    </figure>
+);
+
     
 }
 

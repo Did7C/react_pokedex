dc6c005d18b52955b8ed16663bc095c8a3b1154e
Echeverria-Didier <didbidean@hotmail.com>
2023-11-12 19:54:38 +0100

    nouvelle correction de l'exercice

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,27 +1,34 @@
-import React from 'react';
+import React from "react";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+const pokemon = pokemonList[1];
 
 function PokemonCard() {
-    
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-  
-  return (
-    <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bubasaur" />
-    <figcaption>Bulbasaur</figcaption>
-    </figure>
-);
-
-    
+  if (pokemon.imgSrc !== undefined) {
+    return (
+      <figure>
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+  } else {
+    return (
+      <figure>
+        <p>???</p>
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+  }
 }
 
 export default PokemonCard;
-
